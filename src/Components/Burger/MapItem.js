36fb8743dad87ac5e314fff036cfd874ea83931b
matthewfelixr <matthewfelix88@gmail.com>
2022-04-06 21:35:14 +0700

    Fixing style for remove button

--- src/Components/Burger/MapItem.js
+++ src/Components/Burger/MapItem.js
@@ -7,7 +7,7 @@ const MapItem = (props) => {
     <h6>{props.label}</h6>
         <div className={style.map_controller}>
             {/* Icon digunakan sebagai button untuk menghapus ingredient */}
-            <BiMinusCircle onClick={props.removeHandler} className={style.remoove_button}/>
+            <BiMinusCircle onClick={props.removeHandler} className={style.remove_button}/>
             {/* Icon digunakan sebagai button untuk menaikkan ingredient, ditambilkan bila props.firstItem= false */}
             {!props.firstItem && <BiChevronUpCircle onClick={props.upHandler} className={style.order_button} />}
             {/* Icon digunakan sebagai button untuk menaikkan ingredient, ditambilkan bila props.firstItem= false */}
